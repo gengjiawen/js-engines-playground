4dab95a0748f59be7db0b8daddd28cf81defe9ff
Jiawen Geng <technicalcute@gmail.com>
2024-09-11 06:15:04 +0000

    fix md format

--- benchmark/benchmark.js
+++ benchmark/benchmark.js
@@ -7,18 +7,6 @@ function parseResults(output) {
 }
 
 function createMarkdownTable(results, commandNames) {
-  let table = '| Benchmark (Higher scores are better) '
-  commandNames.forEach((name) => {
-    table += `| ${name} Result `
-  })
-  table += '|\n'
-
-  table += '|-------------------------------------'
-  commandNames.forEach(() => {
-    table += '|------------------'
-  })
-  table += '|\n'
-
   const benchmarks = new Set()
 
   commandNames.forEach((name) => {
@@ -27,12 +15,40 @@ function createMarkdownTable(results, commandNames) {
     })
   })
 
-  benchmarks.forEach((benchmark) => {
-    const row = [`| ${benchmark.padEnd(36)}`]
-    commandNames.forEach((name) => {
+  const benchmarkArray = Array.from(benchmarks)
+  const columnWidths = [Math.max(...benchmarkArray.map((b) => b.length), 37)]
+
+  commandNames.forEach((name) => {
+    const maxLength = Math.max(
+      ...results[name].map((entry) => entry.result.toString().length),
+      name.length,
+      17
+    )
+    columnWidths.push(maxLength)
+  })
+
+  let table = '| Benchmark (Higher scores are better)  '
+  commandNames.forEach((name, index) => {
+    table += `| ${name.padEnd(columnWidths[index + 1])} `
+  })
+  table += '|\n'
+
+  table += '|-' + '-'.repeat(columnWidths[0]) + '-'
+  commandNames.forEach((_, index) => {
+    table += '|-' + '-'.repeat(columnWidths[index + 1]) + '-'
+  })
+  table += '|\n'
+
+  benchmarkArray.forEach((benchmark) => {
+    const row = [`| ${benchmark.padEnd(columnWidths[0])}`]
+    commandNames.forEach((name, index) => {
       const result = results[name].find((entry) => entry.name === benchmark)
       row.push(
-        `| ${result ? result.result.toString().padEnd(16) : 'N/A'.padEnd(16)}`
+        `| ${
+          result
+            ? result.result.toString().padEnd(columnWidths[index + 1])
+            : 'N/A'.padEnd(columnWidths[index + 1])
+        }`
       )
     })
     table += `${row.join(' ')} |\n`
@@ -67,6 +83,7 @@ function createCSV(results, commandNames) {
 
 const commands = [
   { name: 'QuickJS', command: 'quickjs combined.js' },
+  { name: 'qjs-calloc', command: 'qjs-calloc combined.js' },
   { name: 'V8 --jitless', command: 'v8 --jitless combined.js' },
   { name: 'V8', command: 'v8 combined.js' },
   { name: 'JSC', command: 'jsc combined.js' },
